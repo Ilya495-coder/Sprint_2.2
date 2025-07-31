310d0205c9f9be978f65022d6869a50c90409d48
Илюха <goncharovson@mail.ru>
2025-07-31 22:59:45 +0300

    Второй коммит

---
+++ src/main/java/model/Food.java
@@ -0,0 +1,36 @@
+package model;
+
+public abstract class Food {
+    protected int amount;            // килограммы
+    protected double price;         // цена за кг
+    protected boolean isVegetarian; // флаг вегетарианского продукта
+
+    public Food(int amount, double price) {
+        this.amount = amount;
+        this.price = price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isVegetarian() {
+        return isVegetarian;
+    }
+
+    public double getTotalPriceWithoutDiscount() {
+        return amount * price;
+    }
+
+    public double getTotalPriceWithDiscount() {
+        return getTotalPriceWithoutDiscount() * (1 - getDiscount());
+    }
+
+    public double getDiscount() {
+        return 0;
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import model.Apple;
+import model.Food;
+import model.Meat;
+import model.constants.Colour;
+import service.ShoppingCart;
+
+public class Main {
+    public static void main(String[] args) {
+        Food meat = new Meat(5, 100.0);
+        Food redApples = new Apple(10, 50.0, Colour.RED);
+        Food greenApples = new Apple(8, 60.0, Colour.GREEN);
+
+        Food[] products = { meat, redApples, greenApples };
+
+        ShoppingCart cart = new ShoppingCart(products);
+
+        System.out.println("Общая сумма без скидки: " + cart.getTotalPriceWithoutDiscount());
+        System.out.println("Общая сумма со скидкой: " + cart.getTotalPriceWithDiscount());
+        System.out.println("Сумма вегетарианских продуктов без скидки: " + cart.getVegetarianPriceWithoutDiscount());
+    }
+}
